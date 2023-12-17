8f63467f25da63b156d3b1a8873f5e959df8600f
huey-J <wkdgus7113@gmail.com>
2023-12-17 17:09:30 +0900

    initial  commit

---
+++ src/main/java/com/yapp/buddycon/BuddyconBatchApplication.java
@@ -0,0 +1,13 @@
+package com.yapp.buddycon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyconBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddyconBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yapp/buddycon/BuddyconBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yapp.buddycon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddyconBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
